befffc05b3ef72236b30ab78774157912cef55db
Şahin Akkaya <sahin@sahinakkaya.dev>
2025-11-11 18:53:43 +0300

    Fix an error

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <script
-          src="https://challenges.cloudflare.com/turnstile/v0/api.js"
-          async
-          defer
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head nonce={this.props.nonce}>
+          <script
+            src="https://challenges.cloudflare.com/turnstile/v0/api.js"
+            async
+            defer
+            nonce={this.props.nonce}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript nonce={this.props.nonce} />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
